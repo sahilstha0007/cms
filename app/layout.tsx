75d484180e1a86faa9ed5ef9849504d4abb9e812
Sahil Shrestha <sahiltuchhe123@gmail.com>
2025-11-18 19:05:20 +0545

    Project with navbar

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Cms',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
